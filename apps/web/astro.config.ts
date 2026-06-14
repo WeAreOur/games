4b56ac2de170ad97b21a3f9a59172fb6ba29ba2b
gatsbimantico <gatsby@MB2M.local>
2026-06-14 15:35:54 +0100

    Update base path

--- apps/web/astro.config.ts
+++ apps/web/astro.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
 export default defineConfig({
+  base: "/games",
   integrations: [react()],
   output: "static",
   vite: {
